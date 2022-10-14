bf438b81434b5d7ef445fc9305277a82efe376a2
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-14 22:35:32 +0300

    iui

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from tkinter import *
 
 root = Tk()
 
+root.title("RemoveBGPython")
+root.geometry('700x600')
+root.minsize(300,220)
+#section barre de menu
+menu_barr =Menu(root,bg="white")
 input_path = 'cl.jpg'
 output_path = 'output.png'
 
-input =Image.open(input_path)
-output = remove(input)
-output.save(output_path)
+#input =Image.open(input_path)
+#output = remove(input)
+#output.save(output_path)
 
-root.tk
+root.mainloop()
