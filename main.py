849eba14987bc76815f56f354ac99b5fe675cc6c
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-14 23:17:49 +0300

    mande

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from importlib.resources import path
+from select import select
+from tkinter import filedialog
 from rembg import remove
 from PIL import Image
 from tkinter import *
@@ -10,8 +12,15 @@ root.geometry('700x600')
 root.minsize(300,220)
 #section barre de menu
 menu_barr =Menu(root,bg="white")
-input_path = 'cl.jpg'
-output_path = 'output.png'
+
+#fonctiion de selection fichier
+def jpg() :
+    file = filedialog.askopenfilename(initialdir="/")
+    
+    
+selectfile =Button(root,bg="orange",border=0,text="select file" ,command=jpg).pack(expand=YES)
+#input_path = 'cl.jpg'
+#output_path = 'output.png'
 
 #input =Image.open(input_path)
 #output = remove(input)
