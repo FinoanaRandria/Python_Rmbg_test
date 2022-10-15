49a662d593b4cb54cf66c81510fd2b3a8b287706
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-15 22:26:15 +0300

    vfinal

--- main.py
+++ main.py
@@ -5,25 +5,16 @@ from rembg import remove
 from PIL import Image
 from tkinter import *
 
-root = Tk()
 
-root.title("RemoveBGPython")
-root.geometry('700x600')
-root.minsize(300,220)
 #section barre de menu
-menu_barr =Menu(root,bg="white")
 
 #fonctiion de selection fichier
-def jpg() :
-    file = filedialog.askopenfilename(initialdir="/")
-    
-    
-selectfile =Button(root,bg="orange",border=0,text="select file" ,command=jpg).pack(expand=YES)
-#input_path = 'cl.jpg'
-#output_path = 'output.png'
 
-#input =Image.open(input_path)
-#output = remove(input)
-#output.save(output_path)
+    
 
-root.mainloop()
+output_path = 'output.png'
+
+input =Image.open("trtrt.jpg")
+output = remove(input)
+output.save(output_path)
+
