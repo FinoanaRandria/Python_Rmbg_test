ea3f342b2d2666d8b8dcc7de49e3f436d5cb1332
FinoanaRandria <niriantsoa.randriambololomanana@esti.mg>
2022-10-24 11:30:50 +0300

    testena

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+from glob import glob
 from importlib.resources import path
 from select import select
 from tkinter import filedialog
 from rembg import remove
 from PIL import Image
 from tkinter import *
-
+import glob
 
 #section barre de menu
-
+files = glob.glob("images/*.jpg")
+len(files)
 #fonctiion de selection fichier
 
     
-
-output_path = 'output.png'
-
-input =Image.open("trtrt.jpg")
-output = remove(input)
-output.save(output_path)
-
+for file in files:
+    input_path = file
+    output_path = input_path.replace('images','result')
+    
+    with open(input_path, 'rb') as i:
+        with open(output_path,'wb') as o:
+            original_image = i.read()
+            output= remove(original_image)
+            o.write(output)
